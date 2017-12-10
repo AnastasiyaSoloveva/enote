8c294c81049a26c71e0a51080d605db05b98a29a
AnastasiiaSoloveva <floflo134@gmail.com>
2017-12-10 15:12:05 +0300

    change dao package with repositories package, add repo classes

--- src/main/java/dao/AbstractDao.java
+++
@@ -1,8 +0,0 @@
-package dao;
-
-public interface AbstractDao<T> {
-    void deleteById(int id);
-    void create(T obj);
-    void update(T obj);
-    T findById(int id);
-}
---
+++ src/main/java/repositories/repo/TagRepo.java
@@ -0,0 +1,4 @@
+package repositories.repo;
+
+public interface TagRepo {
+}
