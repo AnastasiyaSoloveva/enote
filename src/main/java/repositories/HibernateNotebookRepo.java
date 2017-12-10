fe8917d7ac88142f89ec61b6ab3b0a4dbf2f76b1
sysloparich <sysloparich@gmail.com>
2017-12-10 22:26:58 +0300

    added tests (should be fixed)

--- src/main/java/repositories/HibernateNotebookRepo.java
+++ src/main/java/repositories/HibernateNotebookRepo.java
@@ -1,4 +1,11 @@
 package repositories;
 
-public class HibernateNotebookRepo {
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import repositories.repo.NotebookRepo;
+
+@Repository
+public class HibernateNotebookRepo implements NotebookRepo{
+
 }
